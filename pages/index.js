30b959e14f1c706b7b2f0cd74a8fdbf5aaffec01
giosantoro-meli <giovanni.santoro@mercadolivre.com>
2021-07-16 10:24:43 -0300

    Followers from github and communities gotten via API

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 function ProfileSideBar(prop){
   return (
-      <Box >
+      <Box as="aside">
         <img src={`https://github.com/${prop.githubUser}.png`} style={{borderRadius: "8px"}}/>
 
         <hr />
@@ -24,29 +24,47 @@ function ProfileSideBar(prop){
 }
 
 export default function Home() {
+  const token = '037a036d149c2bf27ce0397376ff7f';
   const githubUser = "giosantoro-meli";
-  const [comunidades, setComunidades] = React.useState([{
-    id: '13123123123',
-    titulo: 'Eu odeio acordar cedo',
-    image: 'https://img10.orkut.br.com/community/52cc4290facd7fa700b897d8a1dc80aa.jpg'
-  },
-  {
-    id: '1312312324234123',
-    titulo: 'Discografias',
-    image: 'https://img10.orkut.br.com/community/f50f08c3f0acf3519578cbc92f81089c.jpg'
-  },
-  {
-    id: '13123123132423443',
-    titulo: 'Eu ofereço comida no MSN',
-    image: 'https://img10.orkut.br.com/community/b0fbba9e4d7b601f153319e23818a106.jpg'
-  },
-  {
-    id: '1312343232423123123',
-    titulo: 'Anão vestido de palhaço mata 8',
-    image: 'https://images-cdn.9gag.com/photo/aKVwG8Z_700b.jpg'
-  }
-  ]);
+  const [comunidades, setComunidades] = React.useState([]);
   const pessoasFavoritas = ['vivian-tanaka', 'matheusmeli', 'rsalewski','AlberAlmeida'];
+  const [seguidores, setSeguidores] = React.useState([]);
+    React.useEffect(function(){
+      fetch('https://api.github.com/users/giosantoro-meli/followers')
+    .then(function(resposta){
+      return resposta.json();
+      })
+    .then(function(respostaCompleta){
+      setSeguidores(respostaCompleta);
+      })
+
+      //API GraphQL
+      fetch('https://graphql.datocms.com/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          query: `query{
+            allCommunities {
+              id
+              title
+              imageUrl
+              creatorSlug
+            }
+          }` 
+        })
+        ,
+      }).then(function(resp){
+        return resp.json();
+      }).then((respCompleta) => {
+        const comunidadesVindasDaApi = respCompleta.data.allCommunities;
+        setComunidades(comunidadesVindasDaApi);
+      })
+    }, []) //this empty array makes this function execute only once
+
   return (
   <>
   <AlurakutMenu githubUser={githubUser} />
@@ -115,32 +133,33 @@ export default function Home() {
     }>
       <ProfileRelationsBoxWrapper>
         <h2 className="smallTitle">
-          Amigos ({pessoasFavoritas.length})
+          Amigos ({seguidores.length})
         </h2>
         <ul>
-        { pessoasFavoritas.map((itemAtual) => {
+        { seguidores.map((itemAtual) => {
           return (
-            <li key={itemAtual}>
-              <a href={`users/${itemAtual}`}>
-                <img src={`https://github.com/${itemAtual}.png`} />
-                <span>{itemAtual}</span>
+            <li key={itemAtual.id}>
+              <a href={`https://github.com/${itemAtual.login}`} target="_blank">
+                <img src={`https://github.com/${itemAtual.login}.png`} />
+                <span>{itemAtual.login}</span>
             </a>
             </li>
           )
         })}
         </ul>
       </ProfileRelationsBoxWrapper>
+        
       <ProfileRelationsBoxWrapper>
         <h2 className="smallTitle">
-          Comunidades
+          Comunidades ({comunidades.length})
         </h2>
         <ul>
         { comunidades.map((itemAtual) => {
           return (
             <li key={itemAtual.id}>
-              <a href={`users/${itemAtual.titulo}`} key={itemAtual.titulo}>
-                <img src={itemAtual.image} />
-                <span>{itemAtual.titulo}</span>
+              <a href={`communities/${itemAtual.id}`}>
+                <img src={itemAtual.imageUrl} />
+                <span>{itemAtual.title}</span>
             </a>
             </li>
           )
